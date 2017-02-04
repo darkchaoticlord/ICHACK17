96f758071dce2c9e854e44b9d2543a287596dd21
ja2416 <jawad.adnan16@ic.ac.uk>
2017-02-04 20:53:22 +0000

    Done some shit

--- Tokenizer/app/src/main/java/com/tokenizer/tokenizer/MainActivity.java
+++ Tokenizer/app/src/main/java/com/tokenizer/tokenizer/MainActivity.java
@@ -1,22 +1,33 @@
 package com.tokenizer.tokenizer;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout drawer;
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setTitle("Tokenizer");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,27 +37,98 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarToggle toggle = new ActionBarToggle(
+                this,                             /* host Activity */
+                drawer,                           /* DrawerLayout object */
+                R.string.navigation_drawer_open,  /* "open drawer" description for accessibility */
+                R.string.navigation_drawer_close  /* "close drawer" description for accessibility */);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        setUpNavigationView();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        switch (item.getItemId()) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            case android.R.id.home:
+                if (drawer.isDrawerOpen(GravityCompat.START)) {
+                    drawer.closeDrawer(GravityCompat.START);
+                } else if (!drawer.isDrawerOpen(GravityCompat.START)) {
+                    drawer.openDrawer(GravityCompat.START);
+                }
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    // Class that sets up a dynamic toolbar for the layout
+    private class ActionBarToggle extends ActionBarDrawerToggle {
+
+        public ActionBarToggle(Activity activity, DrawerLayout drawer, int open, int close) {
+            super(activity, drawer, open, close);
         }
 
-        return super.onOptionsItemSelected(item);
+        // Changes the title of the layout if the drawer is opened.
+        @Override
+        public void onDrawerOpened(View drawerView) {
+            super.onDrawerOpened(drawerView);
+            toolbar.setTitle("Options");
+        }
+
+        // Change the title of the layout back to what it was when it is closed.
+        @Override
+        public void onDrawerClosed(View drawerView) {
+            super.onDrawerClosed(drawerView);
+            toolbar.setTitle("Tokenizer");
+        }
+    }
+
+    // This method is here to set up the functionality of the navigation view layout.
+    private void setUpNavigationView(){
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem item) {
+
+                        // The programs gets the ID of the item selected by the user
+                        // and then decides what to do for item clicked.
+                        int id = item.getItemId();
+                        if (id == R.id.settings_tab) {
+                            Intent openSettings = new Intent(MainActivity.this, SettingsActivity.class);
+                            startActivity(openSettings);
+                        } else if (id == R.id.about_tab) {
+                            Intent openAbout = new Intent(MainActivity.this, );
+                            startActivity(openAbout);
+                        }
+
+                        // After the item in the navigation drawer is clicked, the drawer is
+                        // closed for the new activity/action to take place.
+                        drawer.closeDrawer(GravityCompat.START);
+                        return true;
+                    }
+                });
     }
 }
